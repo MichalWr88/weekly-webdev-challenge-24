88ef6b77feeaa4196a81e3d6feae7c6ef2ba03b9
MichalWr88 <maleszewski.geo@gmail.com>
2018-11-12 15:23:37 +0100

    Revert "style slider end"
    This reverts commit c32a14889bb7ac1f06e6f8ff4776c7ce2e0682d3.

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ const config = {
 	cssoutname: 'style-min.css',
 	jsoutname: 'script-min.js',
 	cssreplaceout: 'css/style-min.css',
-	jsreplaceout: 'js/script-min.js',
+	jsreplaceout: 'js/script-min.js'
 };
 
 /*--------------------------------------------------------*/
@@ -46,32 +46,30 @@ gulp.task('reload', () => {
 
 gulp.task('serve', ['sass'], () => {
 	browserSync({
-		server: config.app,
-	});
+		server: config.app
+	})
 });
+
 
 /*-----------------------------------------------------------------*/
 gulp.watch([config.htmlin], ['reload']);
 gulp.watch(config.scssin, ['sass']);
 gulp.watch(config.jsin, ['babel']);
 
-gulp.task('sass', function() {
-	return (
-		gulp
-			.src(config.scssin)
-			.pipe(sourcemaps.init())
-			.pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-			// outputStyle: 'expanded',  rodzaj formatowania css
-			// sourceComments: 'map'    czy wypisywac zrodla do scss
-			.pipe(
-				autoprefixer({
-					browsers: ['last 3 versions'],
-				})
-			)
-			.pipe(sourcemaps.write()) /*gulp sourcemap*/
-			.pipe(gulp.dest(config.scssout))
-			.pipe(browserSync.stream())
-	);
+
+gulp.task("sass", function () {
+	return gulp.src(config.scssin)
+		.pipe(sourcemaps.init())
+		.pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+		// outputStyle: 'expanded',  rodzaj formatowania css
+		// sourceComments: 'map'    czy wypisywac zrodla do scss
+		.pipe(autoprefixer({
+			browsers: ['last 3 versions']
+		}))
+		.pipe(sourcemaps.write()) /*gulp sourcemap*/
+		.pipe(gulp.dest(config.scssout))
+		.pipe(browserSync.stream());
+
 });
 
 function logFileHelpers() {
@@ -86,15 +84,15 @@ function handleError(error) {
 	this.emit('end');
 }
 
+
+
+
 gulp.task('babel', () => {
-	gulp
-		.src(config.jsin)
+	gulp.src(config.jsin)
 		.pipe(sourcemaps.init())
-		.pipe(
-			babel({
-				presets: ['env'],
-			})
-		)
+		.pipe(babel({
+			presets: ['env'],
+		}))
 		.on('error', handleError)
 		//      .on('error', (err) => {
 		//   // gutil.log(gutil.colors.red('[Compilation Error]'));
@@ -108,41 +106,36 @@ gulp.task('babel', () => {
 		.pipe(browserSync.stream());
 });
 
+
 // FOLDER DIST
 
+
 //minifikacja html------------------------------
-gulp.task('html', function() {
-	return gulp
-		.src(config.htmlin)
-		.pipe(
-			htmlReplace({
-				css: config.cssreplaceout,
-				js: config.jsreplaceout,
-			})
-		)
-		.pipe(
-			htmlMin({
-				sortAttributes: true,
-				sortClassName: true,
-				// collapseWhitespace: true
-			})
-		)
-		.pipe(gulp.dest(config.dist));
+gulp.task('html', function () {
+	return gulp.src(config.htmlin)
+		.pipe(htmlReplace({
+			'css': config.cssreplaceout,
+			'js': config.jsreplaceout
+		}))
+		.pipe(htmlMin({
+			sortAttributes: true,
+			sortClassName: true,
+			// collapseWhitespace: true
+		}))
+		.pipe(gulp.dest(config.dist))
 });
 
 // minifikacja CSS--------------------------------------
-gulp.task('css', function() {
-	return gulp
-		.src(config.cssin)
+gulp.task('css', function () {
+	return gulp.src(config.cssin)
 		.pipe(concat(config.cssoutname)) //nadanie nazwy pliku sconcatenowanu
 		.pipe(cleanCSS())
 		.pipe(gulp.dest(config.cssout));
 });
 
 // minifikacja JS--------------------------------------
-gulp.task('js', function() {
-	return gulp
-		.src(config.js5in)
+gulp.task('js', function () {
+	return gulp.src(config.js5in)
 		.pipe(concat(config.jsoutname))
 		.pipe(uglify())
 		.pipe(gulp.dest(config.jsout));
@@ -157,15 +150,19 @@ gulp.task('js', function() {
 //     .pipe(gulp.dest(config.imgout));
 // });
 
+
+
+
 // run delete ---------------------------
 
 gulp.task('clean', () => {
-	del([config.dist]);
+	del([config.dist])
 });
 // run sequence---------------------------
 
 gulp.task('build', () => {
 	sequence('clean', 'css', 'js', 'html');
 });
+
 
 gulp.task('default', ['serve']);
